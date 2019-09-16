3b6456469711e9bbb0ca1604b9156e6fce3ff63c
hans <nerearubioblasco@gmail.com>
2019-09-16 13:24:56 +0200

    PAYF-183 [DIALOGFLOW] Fill all existing Intents
    * Welcome intent

--- src/app/intents/DefaultWelcomeIntent.class.php
+++ src/app/intents/DefaultWelcomeIntent.class.php
@@ -5,6 +5,7 @@ namespace Solidjobs\Intent\Intents;
 use Solidjobs\Intent\IntentInterface;
 use Solidjobs\Intent\IntentModels\IntentPayLoadModel;
 use Solidjobs\Intent\IntentModels\ResponseModel;
+use Solidjobs\Intent\Services\SolidjobsAppService;
 
 class DefaultWelcomeIntent implements IntentInterface
 {
@@ -21,11 +22,39 @@ class DefaultWelcomeIntent implements IntentInterface
         $intentPayLoad = new IntentPayLoadModel();
 
         /**
+         * Check if there's some personal data set
+         */
+        if ($this->hasData()) {
+            // hi, i see you've some data on your CV,
+            // your cv will be overridden with the data that you provide on this session.
+            // what's your professional position? Student, worker, gardener...
+            $intentPayLoad->setFulfillmentText('');
+            $intentPayLoad->setFulfillmentMessages(['text' => ['message' => '']]);
+        } else {
+            // default fulfillment
+            $intentPayLoad->setFulfillmentText($intentModel->getQueryResult()->getFullfillmentText());
+            $intentPayLoad->setFulfillmentMessages($intentModel->getQueryResult()->getFullfillmentMessages());
+        }
+
+        /**
          * Return the response from DialogFlow
          */
-        $intentPayLoad->setFulfillmentText($intentModel->getQueryResult()->getFullfillmentText());
-        $intentPayLoad->setFulfillmentMessages($intentModel->getQueryResult()->getFullfillmentMessages());
-
         return $intentPayLoad;
+    }
+
+    /**
+     * @return bool
+     */
+    private function hasData()
+    {
+        /**
+         * Check if some section has some data
+         */
+        $hasData = count(SolidjobsAppService::getInstance()->getJobExperiences()) ||
+            count(SolidjobsAppService::getInstance()->getTraining()) ||
+            count(SolidjobsAppService::getInstance()->getAbilities()) ||
+            count(SolidjobsAppService::getInstance()->getLanguages());
+
+        return $hasData;
     }
 }
