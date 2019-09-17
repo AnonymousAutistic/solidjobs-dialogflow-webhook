b56667f3576b2459d5fb5bff70eeb4745ff5b3b6
Hans <nerearubioblasco@gmail.com>
2019-09-17 07:34:34 +0200

    PAYF-183 [DIALOGFLOW] Fill all existing Intents
    Add context actions for each action skeleton on SaveByContextIntent

--- src/app/intents/SaveByContextIntent.class.php
+++ src/app/intents/SaveByContextIntent.class.php
@@ -20,6 +20,7 @@ class SaveByContextIntent implements IntentInterface
     /**
      * @param ResponseModel $intentModel
      * @return IntentPayLoadModel
+     * @throws \Throwable
      */
     public function runIntent(ResponseModel $intentModel)
     {
@@ -59,6 +60,7 @@ class SaveByContextIntent implements IntentInterface
     /**
      * @param $context
      * @param $value
+     * @throws \Throwable
      */
     private function dispatch($context, $value)
     {
@@ -67,12 +69,54 @@ class SaveByContextIntent implements IntentInterface
          */
         if (strpos($context[1], 'got_cv_personal_data_') === 0) {
             $this->saveCVPersonalData($context, $value);
-        } else if(strpos($context[1], 'add_cv_job_experience') === 0) {
+        } else if (strpos($context[1], 'add_cv_job_experience') === 0) {
             $this->addCVJobExperience();
-        } else if(strpos($context[1], 'got_cv_job_experience_') === 0) {
+        } else if (strpos($context[1], 'got_cv_job_experience_') === 0) {
             $this->saveCVJobExperience($context, $value);
         }
     }
+
+    // region add
+
+    /**
+     * OUTPUT_CONTEXT = add_cv_job_experience
+     */
+    private function addCVJobExperience()
+    {
+        /** Create a new empty job experience */
+        SolidjobsAppService::getInstance()->addJobExperience([]);
+    }
+
+    /**
+     * OUTPUT_CONTEXT = add_cv_training
+     */
+    private function addCVTraining()
+    {
+        /** Create new empty training */
+        SolidjobsAppService::getInstance()->addTraining([]);
+    }
+
+    /**
+     * OUTPUT_CONTEXT = add_cv_ability
+     */
+    private function addCVAbility()
+    {
+        /** Create new empty CVAbility */
+        SolidjobsAppService::getInstance()->addAbility([]);
+    }
+
+    /**
+     * OUTPUT_CONTEXT = add_cv_language
+     */
+    private function addCVLanguage()
+    {
+        /** Create new CVLanguage */
+        SolidjobsAppService::getInstance()->addLanguage([]);
+    }
+
+    // endregion add
+
+    // region edit
 
     /**
      * OUTPUT_CONTEXT = got_cv_personal_data_
@@ -89,19 +133,11 @@ class SaveByContextIntent implements IntentInterface
     }
 
     /**
-     * OUTPUT_CONTEXT = add_cv_job_experience
-     */
-    private function addCVJobExperience()
-    {
-        /** Create a new empty job experience */
-        SolidjobsAppService::getInstance()->addJobExperience([]);
-    }
-
-    /**
      * OUTPUT_CONTEXT = got_cv_job_experience_
      *
      * @param $context
      * @param $value
+     * @throws \Throwable
      */
     private function saveCVJobExperience($context, $value)
     {
@@ -115,4 +151,88 @@ class SaveByContextIntent implements IntentInterface
 
         SolidjobsAppService::getInstance()->editJobExperience($id, [$field => $value]);
     }
+
+    /**
+     * OUTPUT_CONTEXT = got_cv_training_
+     *
+     * @param $context
+     * @param $value
+     * @throws \Throwable
+     */
+    private function saveCVTraining($context, $value)
+    {
+        /** Get id from last CVTrainingExperience */
+        $training = SolidjobsAppService::getInstance()->getTraining();
+        $lastTraining = $training[count($training) - 1];
+        $id = $lastTraining['id'];
+
+        /** Remove prefix */
+        $field = str_replace('got_cv_personal_data_', '', $context);
+
+        SolidjobsAppService::getInstance()->editTraining($id, [$field => $value]);
+    }
+
+    /**
+     * OUTPUT_CONTEXT = got_cv_ability_
+     *
+     * @param $context
+     * @param $value
+     * @throws \Exception
+     */
+    private function saveCVAbility($context, $value)
+    {
+        // @todo
+        throw new \Exception();
+    }
+
+    /**
+     * OUTPUT_CONTEXT = got_cv_language_
+     *
+     * @param $context
+     * @param $value
+     * @throws \Exception
+     */
+    private function saveCVLanguage($context, $value)
+    {
+        // @todo
+        throw new \Exception();
+    }
+
+    // endregion edit
+
+    // region delete
+
+    /**
+     * OUTPUT_CONTEXT = delete_all_cv_job_experience
+     */
+    private function deleteAllJobExperience()
+    {
+        // @todo
+    }
+
+    /**
+     * OUTPUT_CONTEXT = delete_all_cv_training
+     */
+    private function deleteAllTraining()
+    {
+        // @todo
+    }
+
+    /**
+     * OUTPUT_CONTEXT = delete_all_cv_ability
+     */
+    private function deleteAllAbility()
+    {
+        // @todo
+    }
+
+    /**
+     * OUTPUT_CONTEXT = delete_all_cv_language
+     */
+    private function deleteAllLanguage()
+    {
+        // @todo
+    }
+
+    // endregion delete
 }
